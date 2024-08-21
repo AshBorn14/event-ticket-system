6851748c18c03891754e805d743a7b7a67a3fabe
Ashborn14 <neon99156@gmail.com>
2024-08-21 18:43:48 +0530

    change download feature

--- src/pages/QrCode.js
+++ src/pages/QrCode.js
@@ -4,6 +4,7 @@ import { IoTicket } from "react-icons/io5";
 import { BsStars } from "react-icons/bs";
 import QRCode from 'qrcode.react';
 import html2canvas from 'html2canvas';
+import domtoimage from 'dom-to-image';
 import jsPDF from 'jspdf';
 
 const Qrcode = () => {
@@ -12,19 +13,35 @@ const Qrcode = () => {
     const qrRef = useRef();
     console.log(data)
 
-    const downloadPDF = () => {
-        html2canvas(qrRef.current).then(canvas => {
-            const imgData = canvas.toDataURL('image/png');
-            const pdf = new jsPDF({
-                orientation: 'portrait',
-                unit: 'px',
-                format: [canvas.width, canvas.height],
-            });
 
-            pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
-            pdf.save(`${data.name}-${data.class}-${data.section}-ticket.pdf`);
+
+    const downloadImage = () => {
+        html2canvas(qrRef.current, {
+            scale: 3,  // Increase the scale for better resolution
+            useCORS: true,  // Use this if your images are from an external source
+            logging: true,  // Enable logging for debugging
+            backgroundColor: null  // Use null to keep the background transparent
+        }).then(canvas => {
+            const link = document.createElement('a');
+            link.href = canvas.toDataURL('image/png');
+            link.download = `${data.name}-${data.class}-${data.section}-ticket.png`;
+            link.click();
         });
     };
+
+    // const downloadPDF = () => {
+    //     html2canvas(qrRef.current).then(canvas => {
+    //         const imgData = canvas.toDataURL('image/png');
+    //         const pdf = new jsPDF({
+    //             orientation: 'portrait',
+    //             unit: 'px',
+    //             format: [canvas.width, canvas.height],
+    //         });
+
+    //         pdf.addImage(imgData, 'PNG', 0, 0, canvas.width, canvas.height);
+    //         pdf.save(`${data.name}-${data.class}-${data.section}-ticket.pdf`);
+    //     });
+    // };
 
 
   return (
@@ -34,11 +51,11 @@ const Qrcode = () => {
 
         </div>
         <div ref={qrRef} className='qr-background mx-auto rounded p-4'>
-            <div className='border-2 border-gray-400 h-full py-2 flex flex-col'>
-                <div className='flex  flex-col items-center'>
+            <div className='border-2 border-gray-400 h-full flex flex-col'>
+                <div className='flex  flex-col items-center mt-2'>
                     <div>
                         <h2 className='text-4xl font-bold text-white font-fam tracking-widest'>After Party</h2>
-                        <p className='text-white -mt-1 text-xs flex justify-center'><span className='mr-2'>Unwind and Celebrate!</span><span className='flex'> <BsStars /> Sept 5</span></p>
+                        <p className='text-white mt-0 text-xs flex justify-center'><span className='mr-2'>Unwind and Celebrate!</span><span className='flex'> <BsStars /> Sept 5</span></p>
                         <h3 className='mt-5 text-white text-3xl text-center font-black tracking-wide'>Entry Pass</h3>
                     </div>
                     
@@ -52,14 +69,14 @@ const Qrcode = () => {
                     
                     
                 </div>
-                <div className='mt-auto text-center text-gray-300 bg-neutral-800 w-full py-2 font-bold'>
-                        <p>Scan the above qr code</p>
+                <div className='mt-auto text-center text-gray-300 py-2 bg-neutral-800 w-full font-bold flex items-center justify-center'>
+                        <p className='text-sm'>Scan the above qr code</p>
                 </div>
 
             </div>
         </div>
         <div className='text-center mb-3'>
-            <button onClick={downloadPDF} className='mt-4 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-2 px-4 rounded'>Download Ticket</button>
+            <button onClick={downloadImage} className='mt-4 text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-2 px-4 rounded'>Download Ticket</button>
         </div>
     </div>
   )
